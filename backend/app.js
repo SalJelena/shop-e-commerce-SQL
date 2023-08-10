4200686e11d5a44e3c4c5def45a8d6d46249c531
SalJelena <salopekjelena24@gmail.com>
2023-08-10 13:34:27 +0200

    edit configuration of db connection

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,25 @@ import express from 'express'
 import { getNote, getNotes, createNote } from './database.js'
 
 const app = express()
+app.use(express.json())
 
 app.get('/notes', async (req, res) => {
     const notes = await getNotes()
     res.send(notes)
+})
+
+app.get('/notes/:id', async (req, res) => {
+    const id = req.params.id 
+    const note = await getNote(id)
+
+    res.send(note)
+})
+
+app.post('/notes', async (req, res) => {
+    const { title, contents } = req.body
+    const note = await createNote(title, contents)
+
+    res.send(note)
 })
 
 app.listen(8080, () => {
