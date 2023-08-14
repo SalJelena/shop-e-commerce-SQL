5c5bc9e771b227c01b6da8fed142ff340b6836f9
SalJelena <salopekjelena24@gmail.com>
2023-08-14 18:24:28 +0200

    edit setup ejs

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,24 @@
-import express from 'express'
+const path = require('path');
 
-import { getNote, getNotes, createNote } from './database.js'
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
-app.use(express.json())
+const errorController = require('./controllers/error')
 
-app.get('/notes', async (req, res) => {
-    const notes = await getNotes()
-    res.send(notes)
-})
+const app = express();
 
-app.get('/notes/:id', async (req, res) => {
-    const id = req.params.id 
-    const note = await getNote(id)
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-    res.send(note)
-})
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.post('/notes', async (req, res) => {
-    const { title, contents } = req.body
-    const note = await createNote(title, contents)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-    res.send(note)
-})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.listen(8080, () => {
-    console.log('Server running on port 8080')
-})
+app.use(errorController.get404Page);
+
+app.listen(3000);
