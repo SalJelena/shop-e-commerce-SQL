0465a60e7e641f2229a6d8fd4211a15ad66ee599
SalJelena <salopekjelena24@gmail.com>
2023-08-25 16:22:32 +0200

    add mysql connection

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node_complete',
-    password: 'Otvorise123.'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 })
 
 module.exports = pool.promise();
