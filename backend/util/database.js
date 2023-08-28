999aead323da7c5d62268ea3383968adc061f6f0
SalJelena <salopekjelena24@gmail.com>
2023-08-28 15:35:55 +0200

    setting up sequelize

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,12 +1,18 @@
-const mysql = require('mysql2')
-const dotenv = require('dotenv')
-dotenv.config()
+// const mysql = require('mysql2')
+// const dotenv = require('dotenv')
+// dotenv.config()
 
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-})
+// const pool = mysql.createPool({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE
+// })
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {dialect: 'mysql', host: 'localhost'})
+
+module.exports = sequelize;
