88ab10c3460b664538fc30cb3681f7c5f702347e
SalJelena <salopekjelena24@gmail.com>
2023-08-30 12:21:07 +0200

    add sequelize queries

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,17 +1,5 @@
-// const mysql = require('mysql2')
-// const dotenv = require('dotenv')
-// dotenv.config()
-
-// const pool = mysql.createPool({
-//     host: process.env.MYSQL_HOST,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE
-// })
-
-// module.exports = pool.promise();
-
-const Sequelize = require('sequelize')
+const {Sequelize} = require('sequelize')
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {dialect: 'mysql', host: 'localhost'})
 
